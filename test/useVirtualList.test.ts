9ae03abac55f904931b47a16b845238febe37c15
wildlifehexagon <crossthebreeze@gmail.com>
2020-06-22 11:20:50 -0500

    test: add unit tests for overscan

--- test/useVirtualList.test.ts
+++ test/useVirtualList.test.ts
@@ -26,4 +26,26 @@ describe("useVirtualList", () => {
       expect(items[10].style.transform).toBe("translateY(300px)")
     })
   })
+
+  describe("items returned with overscan", () => {
+    const { result } = renderHook(() =>
+      useVirtualList({
+        ref,
+        rowHeight: 30,
+        numItems: 100,
+        viewportHeight: 300,
+        overscan: 2,
+      }),
+    )
+    const items = result.current.items
+    it("should return correct amount of items", () => {
+      // same as previous test but with two rows below
+      expect(items.length).toBe(13)
+    })
+    it("should have correctly calculated transform values", () => {
+      expect(items[0].style.transform).toBe("translateY(0px)")
+      expect(items[10].style.transform).toBe("translateY(300px)")
+      expect(items[12].style.transform).toBe("translateY(360px)")
+    })
+  })
 })
