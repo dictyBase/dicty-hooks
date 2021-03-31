af1af2c5befe0047d6e44272b3501fad3fe5cf8a
wildlifehexagon <hartline.eric@gmail.com>
2021-03-31 15:22:44 -0500

    test: update tests to match new hook design

--- test/useIntersectionObserver.test.ts
+++ test/useIntersectionObserver.test.ts
@@ -2,49 +2,32 @@ import { renderHook } from "@testing-library/react-hooks"
 import useIntersectionObserver from "../src/hooks/useIntersectionObserver"
 
 describe("useIntersectionObserver", () => {
-  let ref = null as any
   const window = global as any
   let mockObserve: jest.Mock
-  let mockUnobserve: jest.Mock
-
-  beforeAll(() => {
-    ref = { current: null }
-  })
+  let mockDisconnect: jest.Mock
 
   describe("observe and unobserve methods", () => {
     beforeEach(() => {
       mockObserve = jest.fn()
-      mockUnobserve = jest.fn()
+      mockDisconnect = jest.fn()
     })
     beforeAll(() => {
       window.IntersectionObserver = jest.fn(() => ({
         observe: mockObserve,
-        unobserve: mockUnobserve,
+        disconnect: mockDisconnect,
       }))
     })
     afterEach(() => {
       jest.clearAllMocks()
     })
-    it("should call observe when ref is passed", () => {
-      renderHook(() =>
-        useIntersectionObserver({
-          ref,
-          hasMore: true,
-          rootMargin: "1px",
-          threshold: 1,
-        }),
-      )
-      expect(mockObserve).toHaveBeenCalledTimes(1)
-      expect(mockUnobserve).toHaveBeenCalledTimes(0)
-    })
 
     it("should call unobserve method only on unmount", () => {
       const { unmount } = renderHook(() =>
-        useIntersectionObserver({ ref, hasMore: true }),
+        useIntersectionObserver({ hasMore: true }),
       )
-      expect(mockUnobserve).toHaveBeenCalledTimes(0)
+      expect(mockDisconnect).toHaveBeenCalledTimes(0)
       unmount()
-      expect(mockUnobserve).toHaveBeenCalledTimes(1)
+      expect(mockDisconnect).toHaveBeenCalledTimes(1)
     })
   })
 
@@ -54,16 +37,15 @@ describe("useIntersectionObserver", () => {
         callback([{ isIntersecting: true }])
         return {
           observe: mockObserve,
-          unobserve: mockUnobserve,
+          disconnect: mockDisconnect,
         }
       })
       const { result } = renderHook(() =>
         useIntersectionObserver({
-          ref,
           hasMore: true,
         }),
       )
-      expect(result.current).toBeTruthy()
+      expect(result.current.intersecting).toBeTruthy()
     })
 
     it("should return false if not intersecting and nothing left to fetch", () => {
@@ -71,13 +53,13 @@ describe("useIntersectionObserver", () => {
         callback([{ isIntersecting: false }])
         return {
           observe: mockObserve,
-          unobserve: mockUnobserve,
+          disconnect: mockDisconnect,
         }
       })
       const { result } = renderHook(() =>
-        useIntersectionObserver({ ref, hasMore: false }),
+        useIntersectionObserver({ hasMore: false }),
       )
-      expect(result.current).toBeFalsy()
+      expect(result.current.intersecting).toBeFalsy()
     })
   })
 })
