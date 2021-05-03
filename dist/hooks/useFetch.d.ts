01ba06f98d4fd390ed8416e0cc8735f94e89c631
wildlifehexagon <hartline.eric@gmail.com>
2021-05-03 15:31:12 -0500

    refactor: add build code via husky script

---
+++ dist/hooks/useFetch.d.ts
@@ -0,0 +1,8 @@
+/**
+ * useFetch is a hook for fetching JSON data.
+ */
+declare const useFetch: (url: string, initialData?: {}) => {
+    data: {};
+    error: boolean;
+};
+export default useFetch;
