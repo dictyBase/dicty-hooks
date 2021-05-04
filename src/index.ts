4c18e2127564184c5bc3db61fa047fd5d9dfc517
wildlifehexagon <hartline.eric@gmail.com>
2021-05-04 16:36:13 -0500

    feat: remove useFooter and useNavbar
    BREAKING CHANGE: useFooter and useNavbar have been removed. It is recommended to use useFetch instead.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { default as useFooter } from "./hooks/useFooter"
-export { default as useNavbar } from "./hooks/useNavbar"
 export { default as useFetchRefreshToken } from "./hooks/useFetchRefreshToken"
 export { default as useIntersectionObserver } from "./hooks/useIntersectionObserver"
 export { default as useVirtualList } from "./hooks/useVirtualList"
