1ad6bef61dc1ff01dc4bd0658939292a69adf784
wildlifehexagon <hartline.eric@gmail.com>
2021-05-03 20:38:24 -0500

    fix: use generics for useFetch

--- dist/hooks/useFetch.d.ts
+++ dist/hooks/useFetch.d.ts
@@ -1,8 +1,9 @@
+declare type UseFetch<T> = {
+    error: boolean;
+    data: T;
+};
 /**
  * useFetch is a hook for fetching JSON data.
  */
-declare const useFetch: (url: string, initialData?: {}) => {
-    data: {};
-    error: boolean;
-};
+declare const useFetch: <T>(url: string, initialData?: T) => UseFetch<T>;
 export default useFetch;
